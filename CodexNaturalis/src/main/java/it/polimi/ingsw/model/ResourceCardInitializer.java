29fe769a929510237eedbe87aebaeccedb1b9591
tomneri <tommaso.neri02@gmail.com>
2024-04-21 18:33:54 +0200

    versione corretta

--- CodexNaturalis/src/main/java/it/polimi/ingsw/model/ResourceCardInitializer.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/model/ResourceCardInitializer.java
@@ -1,9 +1,9 @@
 package it.polimi.ingsw.model;
 
-
-import java.util.ArrayList;
-import java.util.List;
+import java.io.FileReader;
+import java.io.IOException;
 import java.io.Reader;
+import java.util.List;
 import javax.json.Json;
 import javax.json.JsonArray;
 import javax.json.JsonObject;
@@ -11,25 +11,25 @@ import javax.json.JsonReader;
 
 public class ResourceCardInitializer {
     private final Game game;
-    //list of all the symbols may be present at card angles
-    //see card manager to get the list
+    // List of all the symbols may be present at card angles
+    // See card manager to get the list
     private final List<Symbol> symbols;
 
-    //constructor
-    public ResourceCardInitializer(Game game, List<Symbol> symbols){
+    // Constructor
+    public ResourceCardInitializer(Game game, List<Symbol> symbols) {
         this.game = game;
         this.symbols = symbols;
     }
 
-    //Initialize cards, creation and call to adder to deck
-    public void initializeResourceCards(){
+    // Initialize cards, creation and call to adder to deck
+    public void initializeResourceCards() {
         try (Reader reader = new FileReader("file.json");
              JsonReader jsonReader = Json.createReader(reader)) {
 
             JsonArray jsonArray = jsonReader.readArray();
 
             for (JsonObject jsonObject : jsonArray.getValuesAs(JsonObject.class)) {
-                // Leggi i valori di ogni oggetto JSON
+                // Read the values of each JSON object
                 String topLeft = jsonObject.getString("topLeft");
                 String topRight = jsonObject.getString("topRight");
                 String bottomLeft = jsonObject.getString("bottomLeft");
@@ -41,28 +41,14 @@ public class ResourceCardInitializer {
                 String reqFox = jsonObject.getString("reqFox");
                 String reqButterfly = jsonObject.getString("reqButterfly");
                 String type = jsonObject.getString("type");
-
-                // Fai qualcosa con i valori, ad esempio stampali a schermo
-                System.out.println("Top Left: " + topLeft);
-                System.out.println("Top Right: " + topRight);
-                System.out.println("Bottom Left: " + bottomLeft);
-                System.out.println("Bottom Right: " + bottomRight);
-                System.out.println("Points For Each: " + pointsForEach);
-                System.out.println("Points: " + points);
-                System.out.println("Required Mushroom: " + reqMushroom);
-                System.out.println("Required Leaf: " + reqLeaf);
-                System.out.println("Required Fox: " + reqFox);
-                System.out.println("Required Butterfly: " + reqButterfly);
-                System.out.println("Type: " + type);
-                System.out.println("----------------------------------");
             }
         } catch (IOException e) {
             e.printStackTrace();
+        }
     }
-    //add the card to the resource deck
-    public void addCardToGame(ResourceCard card){
+
+    // Add the card to the resource deck
+    public void addCardToGame(ResourceCard card) {
         game.addResourceCardToDeck(card);
     }
-
-
 }
