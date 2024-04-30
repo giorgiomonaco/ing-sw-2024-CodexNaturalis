356131c107a33716e26024fac73bffd2d65598cb
fabio.montuschi <fabio.montuschi@mail.polimi.it>
2024-04-30 12:39:05 +0200

    cambio nome Angle

---
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/model/Angle.java
@@ -0,0 +1,34 @@
+package it.polimi.ingsw.model;
+
+
+public class Angle {
+    //every visible VisibleAngle has a symbol in it,
+    //It may be null, so the VisibleAngle is empty
+    //!!!!!!!!!!!!!!!!!!Don't like this class cus only subclass of VisibleAngle,
+    //may be useless have a distinction
+    private final Symbol symbol;
+
+    //attribute tha determines if the VisibleAngle is visible or not,
+    //used to check how and which resources are available
+    private boolean notCovered = true;
+
+    //constructor
+    public Angle(Symbol symbol){
+        this.symbol = symbol;
+    }
+
+    //get of the symbol
+    public Symbol getSymbol() {
+        return symbol;
+    }
+
+    //get if VisibleAngle covered or not
+    public boolean isNotCovered() {
+        return notCovered;
+    }
+
+    //change the state of the coverage of the VisibleAngle
+    public void setNotCovered(boolean notCovered) {
+        this.notCovered = notCovered;
+    }
+}
