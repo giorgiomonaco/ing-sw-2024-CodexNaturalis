70f8cd14636add9845fa6c842c03f77c4838850e
tommaso <tommaso.neri@mail.polimi.it>
2024-04-30 12:37:43 +0200

    rimozione angle astratta

--- CodexNaturalis/src/main/java/it/polimi/ingsw/model/Card.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/model/Card.java
@@ -12,11 +12,11 @@ public abstract class Card {
     //1 = bottom right
     //2 = bottom left
     //3 = top left
-    private final Angle[] frontAngles = new Angle[4];
+    private final VisibleAngle[] frontAngles = new VisibleAngle[4];
 
     //array representing the 4 angles of the back of the card
     //same enumeration
-    private final Angle[] backAngles = new Angle[4];
+    private final VisibleAngle[] backAngles = new VisibleAngle[4];
 
     //central symbol on the back of the card
     private final Symbol backSymbol;
@@ -28,7 +28,7 @@ public abstract class Card {
     private boolean frontSide;
 
     //Constructor
-    public Card(Angle[] frontAngles, Angle[] backAngles, Symbol backSymbol){
+    public Card(VisibleAngle[] frontAngles, VisibleAngle[] backAngles, Symbol backSymbol){
         for (int i = 0; i < 4; i++ ){
             this.frontAngles[i] = frontAngles[i];
             this.backAngles[i] = backAngles[i];
@@ -37,12 +37,12 @@ public abstract class Card {
     }
 
     //get of the front angles
-    public Angle[] getFrontAngles() {
+    public VisibleAngle[] getFrontAngles() {
         return frontAngles;
     }
 
     //get of the back angles
-    public Angle[] getBackAngles() {
+    public VisibleAngle[] getBackAngles() {
         return backAngles;
     }
 
