c89ba003e6441101ae6175b3b5b90635e7c48934
tommaso <tommaso.neri@mail.polimi.it>
2024-04-30 12:41:40 +0200

    rimozione angle astratta

--- CodexNaturalis/src/main/java/it/polimi/ingsw/model/Card.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/model/Card.java
@@ -64,4 +64,16 @@ public abstract class Card {
     public TIFFField get(String topLeft) {
         return null;
     }
+
+    public VisibleAngle getVisibleAngleFront(int position){
+        if (frontAngles[position] != null){
+            return frontAngles[position];
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+
 }
