551831be685d1e8cfddf1717db9c061bab5816a5
tomneri <tommaso.neri02@gmail.com>
2024-05-13 23:38:30 +0200

    view implements with colors

--- CodexNaturalis/src/main/java/it/polimi/ingsw/client/view/TUI/TuiViews/AlreadyStartedView.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/client/view/TUI/TuiViews/AlreadyStartedView.java
@@ -3,6 +3,6 @@ package it.polimi.ingsw.client.view.TUI.TuiViews;
 public class AlreadyStartedView implements TuiView{
     @Override
     public void start() {
-
+        System.out.println("YOU CANNOT JOIN THE GAME, WAIT FOR THE CURRENT GAME TO END");
     }
 }
