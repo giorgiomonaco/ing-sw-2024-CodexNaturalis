4387cd9ab2ef908e42d4c5ce0c716923379f28d3
giorgiomonaco <giorgio.monaco@mail.polimi.it>
2024-05-20 19:24:54 +0200

    addiction of a new Exception

--- CodexNaturalis/src/main/java/it/polimi/ingsw/client/commandsHandling/CommandManager.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/client/commandsHandling/CommandManager.java
@@ -1,12 +1,13 @@
 package it.polimi.ingsw.client.commandsHandling;
 
 import it.polimi.ingsw.client.commandsHandling.commandsException.CommandNotAvailableException;
+import it.polimi.ingsw.client.commandsHandling.commandsException.WrongInsertionException;
 import it.polimi.ingsw.client.states.stateEnum;
 
 import java.rmi.RemoteException;
 
 public interface CommandManager {
 
-    public void handleMessage(String[] commands, stateEnum clientState) throws RemoteException, CommandNotAvailableException;
+    public void handleMessage(String[] commands, stateEnum clientState) throws RemoteException, CommandNotAvailableException, WrongInsertionException;
 
 }
