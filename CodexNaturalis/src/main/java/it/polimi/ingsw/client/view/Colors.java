285fefd0663ce968c28aa17b4aba7b865ea7bd0c
giorgiomonaco <giorgio.monaco@mail.polimi.it>
2024-05-21 00:09:53 +0200

    setting of the colors

--- CodexNaturalis/src/main/java/it/polimi/ingsw/client/view/Colors.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/client/view/Colors.java
@@ -3,6 +3,22 @@ package it.polimi.ingsw.client.view;
 import java.io.Serializable;
 
 public class Colors {
+
+    public String getColor(String s){
+
+        String result;
+
+        switch(s){
+            case "red" -> result = redColor;
+            case "green" -> result = greenColor;
+            case "yellow" -> result = yellowColor;
+            case "blue" -> result = blueColor;
+            case "black" -> result = blackColor;
+            default -> result = "";
+        }
+
+        return result;
+    }
     public String redColor = "\u001B[31m";
     public String greenColor = "\u001B[32m";
     public String yellowColor = "\u001B[33m";
--- CodexNaturalis/src/main/java/it/polimi/ingsw/client/view/TUI/TuiViews/SelTokenView.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/client/view/TUI/TuiViews/SelTokenView.java
@@ -2,15 +2,24 @@ package it.polimi.ingsw.client.view.TUI.TuiViews;
 import it.polimi.ingsw.client.Client;
 import it.polimi.ingsw.client.view.Colors;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SelTokenView implements TuiView{
     Colors color = new Colors();
+    List<String> availableTokens = new ArrayList<>();
     @Override
     public void play(Client client) {
 
-        System.out.println("WHICH COLOUR DO YOU WANT TO PLAY WITH?\nCHOOSE BETWEEN: ");
-        System.out.print(color.redColor + " red " + color.resetColor);
-        System.out.print(color.blueColor + " blue " + color.resetColor);
-        System.out.print(color.yellowColor + " yellow " + color.resetColor);
-        System.out.print(color.greenColor + " green " + color.resetColor);
+        System.out.println("\n+----------------------+\n" +
+                "|      GAME SET UP     |\n" +
+                "+----------------------+\n\nSelect your token from the available ones:\n");
+        availableTokens = client.getAvailableTokens();
+
+        for (String s: availableTokens) {
+            System.out.print(color.getColor(s) + s + color.resetColor + " ");
+        }
+
+        System.out.println("\n\nInsert color <sel> where sel is the selected color.");
     }
 }
