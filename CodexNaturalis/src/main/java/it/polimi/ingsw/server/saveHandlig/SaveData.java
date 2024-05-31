73e2089889ed547af1e23a209d14db1eeeb8e3ba
giorgiomonaco <giorgio.monaco@mail.polimi.it>
2024-05-31 17:14:10 +0200

    starting setup saving data

---
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/server/saveHandlig/SaveData.java
@@ -0,0 +1,13 @@
+package it.polimi.ingsw.server.saveHandlig;
+
+import it.polimi.ingsw.server.model.Player;
+
+import java.util.List;
+
+/**
+ * The class contains the information needed to save and load an existing game.
+ */
+public class SaveData {
+    private List<Player> players;
+
+}
---
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/server/saveHandlig/SaveManager.java
@@ -0,0 +1,8 @@
+package it.polimi.ingsw.server.saveHandlig;
+
+/**
+ * The class manages the information needed to save and load an existing game.
+ */
+public class SaveManager {
+
+}
