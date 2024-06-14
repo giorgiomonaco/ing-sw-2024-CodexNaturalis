04daabacff251611b6dc8915215c2617fe0e1a6e
giorgiomonaco <giorgio.monaco@mail.polimi.it>
2024-06-14 17:44:39 +0200

    implementig gui

--- CodexNaturalis/src/main/java/it/polimi/ingsw/client/view/GUI/Gui.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/client/view/GUI/Gui.java
@@ -1,12 +1,14 @@
 package it.polimi.ingsw.client.view.GUI;
 
 import it.polimi.ingsw.client.Client;
+import it.polimi.ingsw.client.view.Colors;
 import it.polimi.ingsw.client.view.GUI.Panels.LoginPanel;
 import it.polimi.ingsw.client.view.UserInterface;
 import it.polimi.ingsw.network.message.Message;
 import it.polimi.ingsw.server.model.Card;
 import it.polimi.ingsw.server.model.InitialCard;
 
+import javax.swing.*;
 import java.awt.*;
 import java.util.List;
 
@@ -22,6 +24,7 @@ public class Gui implements UserInterface {
 
     public Gui (Client client){
         this.client = client;
+        client.setUI(this);
         gbl = new GridBagLayout();
         gbc = new GridBagConstraints();
 
@@ -33,6 +36,9 @@ public class Gui implements UserInterface {
             case LOGIN:
                 createFrame();
                 addLoginPanel();
+                break;
+            case LOGIN_SUCCESSFUL:
+                printMessage("Login successful!");
                 break;
             default:
                 break;
@@ -59,17 +65,30 @@ public class Gui implements UserInterface {
 
     }
 
+    public void printMessage(String s) {
+        gbc = new GridBagConstraints();
+        JPanel message = new JPanel();
+        message.setSize(new Dimension(400, 200));
+        JLabel label = new JLabel(s);
+        gbc.gridx = 0;
+        gbc.gridy = 1;
+        gbc.anchor = GridBagConstraints.CENTER;
+        message.add(label);
+        frame.add(message, gbc);
+        frame.setVisible(true);
+    }
+
 
     private void createFrame(){
-        frame = new MyFrame(gbl);
+        frame = new MyFrame();
     }
 
     private void addLoginPanel(){
         LoginPanel logPanel = new LoginPanel(gbl, client);
-        //now we add it to the frame
         gbc.gridx = 0;
         gbc.gridy = 0;
-
+        gbc.anchor = GridBagConstraints.NORTH;
+        //now we add it to the frame
         frame.add(logPanel,gbc);
         //we make the panel visible to be displaced
         frame.setVisible(true);
--- CodexNaturalis/src/main/java/it/polimi/ingsw/client/view/GUI/MyFrame.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/client/view/GUI/MyFrame.java
@@ -7,15 +7,13 @@ public class MyFrame extends JFrame {
     layout and constraints ot the layout
     used to place in right place all the elements of the frame
      */
-    private GridBagLayout layout;
-    private GridBagConstraints gbc;
 
     //Dimensions of the frame
     private static final int DIM_X = 1000;
     private static final int DIM_Y = 600;
-    public MyFrame(GridBagLayout layout){
-        this.layout = layout;
-        gbc = new GridBagConstraints();
+
+    public MyFrame(){
+        setLayout(new GridBagLayout());
         setSize(new Dimension(DIM_X,DIM_Y));
         setTitle("CODEX NATURALIS");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
